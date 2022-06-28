9c0336c5e94a19758536d7f8e658a2e4599d30d7
Aditya Raj <72968234+adityaRajGit@users.noreply.github.com>
2022-06-28 19:06:38 +0530

    Time: 26 ms (10.29%), Space: 5.8 MB (79.76%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int l=0;
+    int fib(int n) {
+        if(n==0)
+            return 0;
+        if(n==1)
+            return 1;
+        else
+            return fib(n-1)+fib(n-2);
+    }
+};
